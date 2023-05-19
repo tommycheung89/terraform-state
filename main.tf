9e706ec7c59f229511fc81a7681c5c645b2dcc28
tommycheung89 <tcheung@palo-it.com>
2023-05-19 11:53:33 +0800

    add line to the instance

--- main.tf
+++ main.tf
@@ -5,4 +5,39 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "teraform-up-and-running-state"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_srver_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorith = "AES256"
+    }
+  }
 }
